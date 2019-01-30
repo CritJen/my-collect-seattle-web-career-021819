039dd6bab2206a9679cc4fd7c61b819c727ec807
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-30 08:55:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+  new_array = []
+  i = 0
 
+  while i < array.length
+    yield(array[i])
+    new_array << array[i].upcase
+    i += 1
+  end
+  new_array
+end
 
+def my_collect(array)
+  new_array = []
+  i = 0
+
+  while i < array.length
+    yield(array[i])
+    new_array << array[i].split(" ").first
+    i += 1
+  end
+  new_array
+end
